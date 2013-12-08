8bb0e643e55f19866515f9cc19a7ed94366b5dd1
Gökçen Eraslan <gokcen.eraslan@gmail.com>
2013-12-08 17:20:17 +0200

    Filter out targets and models having residues without calpha atoms.

--- tests/generateCASP10csv.py
+++ tests/generateCASP10csv.py
@@ -4,7 +4,10 @@ import csv
 import os
 import sys
 
+from prody import *
+
 caspdir = sys.argv[1]
+confProDy(verbosity='none')
 
 with open(sys.argv[2], 'w') as csvfile:
     writer = csv.writer(csvfile, delimiter=',', quotechar='"')
@@ -16,8 +19,30 @@ with open(sys.argv[2], 'w') as csvfile:
         else:
             target = os.path.basename(root)
             targetfile = os.path.join(root, target + '.pdb')
+
             assert os.path.isfile(targetfile), ('Target %s cannot be found' %
                                                 targetfile)
+
+            targetPDB = parsePDB(targetfile)
+            if targetPDB.numResidues() != targetPDB.ca.numAtoms():
+                   print "Target %s is invalid" % targetfile
+                   print "Numresidues: %d vs. numCA:%d" % \
+                   (targetPDB.numResidues(), targetPDB.ca.numAtoms())
+                   continue
+
             files.remove(os.path.basename(targetfile))
             files.sort()
-            writer.writerow([targetfile] + [os.path.join(root, f) for f in files])
+            files = [os.path.join(root, f) for f in files]
+            filesFiltered = []
+            for f in files:
+                fPDB = parsePDB(f)
+                if not fPDB:
+                    print 'Invalid PDB %s, skipping...' % f
+                    continue
+
+                if fPDB.numResidues() == fPDB.ca.numAtoms():
+                    filesFiltered.append(f)
+                else:
+                    print 'Invalied number of CA in %s, skipping...' % f
+
+            writer.writerow([targetfile] + filesFiltered)
